e12f7176d8254cee9e486a78ed9e0cb05e34e2d1
elias <egutayi@shamvagold.co.zw>
2024-10-14 16:21:53 +0200

    navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import css from "./navbar.css"
+import React from 'react';
+import css from "./navbar.css";
+import logo from "../../assets/logo.png" ;
+import contactImg from '../../assets/contact.png';
+import {Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <div>
-      navbar
+      <nav className='navbar'>
+        <img src= {logo} alt='' className='logo' />
+        <div className='desktopMenu'>
+          <Link className='desktopMenuListItem'>home</Link>
+          <Link className='desktopMenuListItem'>about</Link>
+          <Link className='desktopMenuListItem'>client</Link>
+          <Link className='desktopMenuListItem'>Portfolio</Link>
+
+
+
+        </div>
+        <button  className='desktopMenuBtn'>
+          <img src='' alt='' className='desktopMenuImg' /> Contact Me</button>
+
+
+
+
+      </nav>
     </div>
   )
 }
